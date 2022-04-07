f41e199b88afc4c22d2ea23241a8009b78d2f0e2
zyp-wsl <2671078523@qq.com>
2022-04-07 11:24:53 +0800

    条件变量的多线程队列

--- design_concurrency/queue_threads_safe.cpp
+++ design_concurrency/queue_threads_safe.cpp
@@ -16,13 +16,39 @@ private:
   std::condition_variable data_cond;
 
 public:
+  threadsafe_queue(){};
+
   void push(T new_value)
   {
     std::lock_guard<std::mutex> lk(mut);
-    
+    data_queue.push(std::move(data));
+    data_cond.notify_one(); // 
   }
-}
 
+  void wait_and_pop(T& value)
+  {
+    std::unique_lock<std::mutex> lk(mut);
+    data_cond.wait(lk, [this]{return !data_queue.empty();});
+    value = std::move(data_queue.front());
+    data_queue.pop();
+  }
 
+  std::shared_ptr<T> wait_and_pop()
+  {
+    std::unique_lock<std::mutex> lk(mut);
+    data_cond.wait(lk, [this]{return ! data_queue.empty(); } );
+    std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
+    data_queue.pop();
+    return res;
+  }
 
-
+  bool try_pop(T& value)
+  {
+    std::lock_guard<std::mutex> lk(mut);
+    if (data_queue.empty())
+      return false;
+    value = std::move(*data_queue.front());
+    data_queue.pop();
+    return true;
+  }
+};
