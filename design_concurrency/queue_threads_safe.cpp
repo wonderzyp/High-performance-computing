025640854f48c03fe4e02c4876bf1375ea46d2e5
zyp-wsl <2671078523@qq.com>
2022-04-11 11:20:37 +0800

    准备实现细粒度的线程安全队列

--- design_concurrency/queue_threads_safe.cpp
+++ design_concurrency/queue_threads_safe.cpp
@@ -7,12 +7,15 @@
 #include <mutex>
 #include <condition_variable>
 
+namespace Coarse_Threadsafe_Queue{
 template <typename T>
 class threadsafe_queue
 {
 private:
-  std::mutex mut;
-  std::queue<T> data_queue;
+  mutable std::mutex mut;
+  //std::queue<T> data_queue;
+  // 将shared_ptr初始化过程移至push中，而不是直接使用数据值
+  std::queue<std::shared_ptr<T>> data_queue;
   std::condition_variable data_cond;
 
 public:
@@ -20,9 +23,11 @@ public:
 
   void push(T new_value)
   {
+    // 将shared_ptr的构造过程移入push函数中
+    std::shared_ptr<T> data( std::make_shared<T>(std::move(new_value)));
     std::lock_guard<std::mutex> lk(mut);
-    data_queue.push(std::move(data));
-    data_cond.notify_one(); // 
+    data_queue.push(data);
+    data_cond.notify_one(); // 使用notify_all, 大量线程被唤醒后发现不符合要求，再次休眠
   }
 
 
@@ -30,17 +35,8 @@ public:
   {
     std::unique_lock<std::mutex> lk(mut);
     data_cond.wait(lk, [this]{return !data_queue.empty();});
-    value = std::move(data_queue.front());
+    value = std::move(*data_queue.front());
     data_queue.pop();
-  }
-
-  std::shared_ptr<T> wait_and_pop()
-  {
-    std::unique_lock<std::mutex> lk(mut);
-    data_cond.wait(lk, [this]{return ! data_queue.empty(); } );
-    std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
-    data_queue.pop();
-    return res;
   }
 
   bool try_pop(T& value)
@@ -53,13 +49,28 @@ public:
     return true;
   }
 
+  std::shared_ptr<T> wait_and_pop()
+  {
+    std::unique_lock<std::mutex> lk(mut);
+    data_cond.wait(lk, [this]{return ! data_queue.empty(); } );
+    // std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
+    
+    std::shared_ptr<T> res = data_queue.front();
+
+    data_queue.pop();
+    return res;
+  }
+
   std::shared_ptr<T> try_pop()
   {
     std::lock_guard<std::mutex> lk(mut);
     if (data_queue.empty())
       return std::shared_ptr<T>();
     
-    std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
+    // std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
+    
+    std::shared_ptr<T> res = data_queue.front();
+    
     data_queue.pop();
     return res;
   }
@@ -69,4 +80,47 @@ public:
     std::lock_guard<std::mutex> lk(mut);
     return data_queue.empty();
   }
+};
+} // namespace Coarse_Threadsafe_Queue
+
+
+// 粗粒度锁对整个queue数据结构进行保护
+// 若需要使用细粒度锁，需对queue内部数据结构进行分析
+namespace Fine_Threadsafe_Queue{
+
+template <typename T>
+class queue
+{
+private:
+  struct node
+  {
+    T data;
+    std::unique_ptr<node> next;
+
+    node(T data_) : data(std::move(data_)){};
+  };
+
+  std::unique_ptr<node> head;
+  node* tail;
+
+public:
+  queue(){};
+  queue(const queue& other) = delete;
+  queue& operator=(const queue& other) = delete;
+
+  std::shared_ptr<T> try_pop(){
+    if (!head){
+      return std::shared_ptr<T>();
+    }
+
+    std::shared_ptr<T> const res(std::make_shared<T>(std::move(head->data)));
+    std::unique_ptr<node> const old_head = std::move(head);
+    head = std::move(old_head->next);
+    return res;
+  }
+};
+
+
+
+
 };
